ce734be0fc1456ab0ecbbddeedf3005482a6c33a
alakaktyc <alakaktyc@gmail.com>
2020-10-13 23:20:05 +0300

    feat: solve brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let count = 0;
+    while (count < bracketsConfig.length) {
+        let kindOfBracket = bracketsConfig[count].join('');
+        if (str.includes(kindOfBracket)) {
+            str = str.replace(kindOfBracket, '');
+            count = 0;
+        } else {
+            count++;
+        }
+    }
+    return !str.length;
+};
